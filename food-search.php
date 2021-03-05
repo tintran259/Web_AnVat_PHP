29f824d510d9319bfb42331754aa293f4c1e6d3f
nguyentiencuong21101999 <56511301+tintran259@users.noreply.github.com>
2021-03-05 12:57:26 +0700

    css Home

--- food-search.php
+++ food-search.php
@@ -6,9 +6,7 @@
     <!-- fOOD sEARCH Section Starts Here -->
     <section class="food-search text-center">
         <div class="container">
-            
             <h2>Foods on Your Search <a href="#" class="text-white">"Momo"</a></h2>
-
         </div>
     </section>
     <!-- fOOD sEARCH Section Ends Here -->
@@ -19,7 +17,6 @@
     <section class="food-menu">
         <div class="container">
             <h2 class="text-center">Food Menu</h2>
-
             <div class="food-menu-box">
                 <div class="food-menu-img">
                     <img src="images/menu-pizza.jpg" alt="Chicke Hawain Pizza" class="img-responsive img-curve">
